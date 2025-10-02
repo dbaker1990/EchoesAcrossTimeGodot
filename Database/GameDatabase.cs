4bef1d25e21c93b59649785762a64ece9d1ca2eb
Scion <dominicbaker16@gmail.com>
2025-10-02 15:23:36 -0700

    database

---
+++ Database/GameDatabase.cs
@@ -0,0 +1,57 @@
+﻿// Database/GameDatabase.cs
+
+using System.Linq;
+using EchoesAcrossTime.Combat;
+using EchoesAcrossTime.Database;
+using EchoesAcrossTime.Items;
+using Godot;
+using Godot.Collections;
+
+[GlobalClass]
+public partial class GameDatabase : Resource
+{
+    [ExportGroup("Characters")]
+    [Export] public Array<CharacterData> PlayableCharacters { get; set; }
+    [Export] public Array<CharacterData> Enemies { get; set; }
+    [Export] public Array<CharacterData> Bosses { get; set; }
+    
+    [ExportGroup("Combat")]
+    [Export] public Array<SkillData> Skills { get; set; }
+    
+    [ExportGroup("Items")]
+    [Export] public Array<ConsumableData> Consumables { get; set; }
+    [Export] public Array<EquipmentData> Equipment { get; set; }
+    
+    public GameDatabase()
+    {
+        PlayableCharacters = new Array<CharacterData>();
+        Enemies = new Array<CharacterData>();
+        Bosses = new Array<CharacterData>();
+        Skills = new Array<SkillData>();
+        Consumables = new Array<ConsumableData>();
+        Equipment = new Array<EquipmentData>();
+    }
+    
+    // Helper methods
+    public CharacterData GetCharacter(string id)
+    {
+        foreach (var character in PlayableCharacters)
+        {
+            if (character?.CharacterId == id) return character;
+        }
+        foreach (var enemy in Enemies)
+        {
+            if (enemy?.CharacterId == id) return enemy;
+        }
+        foreach (var boss in Bosses)
+        {
+            if (boss?.CharacterId == id) return boss;
+        }
+        return null;
+    }
+    
+    public SkillData GetSkill(string id)
+    {
+        return Skills.FirstOrDefault(s => s?.SkillId == id);
+    }
+}
---
+++ addons/rpg_database/RPGDatabasePlugin.cs
@@ -0,0 +1,22 @@
+﻿// addons/rpg_database/RPGDatabasePlugin.cs
+#if TOOLS
+using Godot;
+
+[Tool]
+public partial class RPGDatabasePlugin : EditorPlugin
+{
+    private Control databasePanel;
+    
+    public override void _EnterTree()
+    {
+        databasePanel = GD.Load<PackedScene>("res://addons/rpg_database/DatabasePanel.tscn").Instantiate<Control>();
+        AddControlToBottomPanel(databasePanel, "RPG Database");
+    }
+    
+    public override void _ExitTree()
+    {
+        RemoveControlFromBottomPanel(databasePanel);
+        databasePanel.QueueFree();
+    }
+}
+#endif
